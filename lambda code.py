d88fc9b4eb9f627ac8e2f437e5313d743ee36c78
Sultan Al Awar <sultan.alawar.20@ucl.ac.uk>
2021-04-27 14:09:23 +0100

    Initial commit

---
+++ lambda code.py
@@ -0,0 +1,29 @@
+import json
+import csv 
+import boto3
+def lambda_handler(event, context):
+    region = 'eu-west-1'
+    record_ddlist = [] 
+    try:
+        s3 = boto3.client('s3') 
+        dynamodb = boto3.client('dynamodb', region_name = region)
+        bucket = event['Records'][0]['s3']['bucket']['name']
+        key = event['Records'][0]['s3']['object']['key']
+        print('Bucket: ', bucket, 'Key: ', key)
+        csv_file = s3.get_object(Bucket=bucket, Key=key)
+        record_list = csv_file["Body"].read().decode('utf-8').split('\n')
+        csv_reader = csv.reader(record_list, delimiter=',', quotechar='"')
+        for row in csv_reader:
+            id = row[0] 
+            text = row[1] 
+            add_to_db = dynamodb.put_item(
+                TableName = 'covid_tweets',
+                Item = {
+                    'id' : {'S': str(id)},
+                    'text' : {'S': str(text)},
+                    })
+            print("Successfully added")
+    except Exception as e:
+        print(str(e))
+        
+
